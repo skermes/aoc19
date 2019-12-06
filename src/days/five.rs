e7844b6d76f9709d19646287c8dbb7f9805a316b
Sean Kermes <skermes@gmail.com>
2019-12-05 21:02:28 -0500

    Solve day 5 part 1

--- src/days/five.rs
+++ src/days/five.rs
@@ -1,10 +1,17 @@
+use itertools::Itertools;
+
 use crate::problem::Problem;
+use crate::intcode::Machine;
 
 pub struct DayFive {}
 
 impl Problem for DayFive {
     fn part_one(&self, input: &str) -> String {
-        format!("{}", "Part one not yet implemented.")
+        let machine = Machine::from_str(input).unwrap();
+        let with_input = machine.write(1);
+        let halted = with_input.run_to_halt().unwrap();
+
+        format!("{}", halted.output.iter().join(" "))
     }
 
     fn part_two(&self, input: &str) -> String {
