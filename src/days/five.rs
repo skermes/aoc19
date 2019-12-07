7e1cb7f0695b78b4dda339202697f4b18801b445
Sean Kermes <skermes@gmail.com>
2019-12-07 14:45:11 -0500

    Do intcode I/O over channels.

--- src/days/five.rs
+++ src/days/five.rs
@@ -12,18 +12,18 @@ impl Problem for DayFive {
 
     fn part_one(&self, input: &str) -> String {
         let mut machine = Machine::from_str(input).unwrap();
-        machine.write(1);
+        machine.input.send(1).unwrap();
         machine.run_to_halt().unwrap();
 
-        format!("{}", machine.output.iter().join(" "))
+        format!("{}", machine.output.try_iter().join(" "))
     }
 
     fn part_two(&self, input: &str) -> String {
         let mut machine = Machine::from_str(input).unwrap();
-        machine.write(5);
+        machine.input.send(5).unwrap();
         machine.run_to_halt().unwrap();
 
-        format!("{}", machine.output.iter().join(" "))
+        format!("{}", machine.output.try_iter().join(" "))
     }
 }
 
