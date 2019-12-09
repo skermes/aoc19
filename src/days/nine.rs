f030873d808ac1cd8184dff6f0aaf26f4f4cdf7c
Sean Kermes <sean@arcbio.com>
2019-12-09 11:58:27 -0500

    Implement day 9 part 2.

--- src/days/nine.rs
+++ src/days/nine.rs
@@ -6,6 +6,10 @@ use crate::intcode::Machine;
 pub struct DayNine {}
 
 impl Problem for DayNine {
+    fn name(&self) -> String {
+        "Sensor Boost".to_string()
+    }
+
     fn part_one(&self, input: &str) -> String {
         let mut machine = Machine::from_str(input).unwrap();
         machine.write(1);
@@ -15,7 +19,11 @@ impl Problem for DayNine {
     }
 
     fn part_two(&self, input: &str) -> String {
-        format!("{}", "Part two not yet implemented.")
+        let mut machine = Machine::from_str(input).unwrap();
+        machine.write(2);
+        machine.run().unwrap();
+
+        machine.read().iter().join(" ").to_string()
     }
 }
 
