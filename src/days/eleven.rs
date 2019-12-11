b36b23add0ec7d436431ca2b721f4e95886b3fca
Sean Kermes <skermes@gmail.com>
2019-12-11 08:50:10 -0500

    Partial implementation of day 11.
    The problem is pretty cagey about whether the brain's i/o
    instructions will always happen in lockstep, so I'm not sure I
    can rely on 2 values being available to read when the brain
    blocks.  I might as well try and see how it goes, but I might
    have to add a peek() method to intcode machines that lets me
    see the available output without consuming it.

--- src/days/eleven.rs
+++ src/days/eleven.rs
@@ -1,4 +1,138 @@
+use std::collections::HashMap;
+
 use crate::problem::Problem;
+use crate::intcode::{Machine,MachineState,OperationalError};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum PaintingError {
+    #[error("`{0}` is not a valid color.")]
+    InvalidColor(isize),
+    #[error("`{0}` is not a valid turn.")]
+    InvalidTurn(isize),
+    #[error("Error encountered while running intcode program.")]
+    IntcodeError(#[from] OperationalError)
+}
+
+type PaintingResult<T> = Result<T, PaintingError>;
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
+struct Point {
+    x: isize,
+    y: isize
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Color {
+    Black,
+    White
+}
+
+impl Color {
+    fn from_int(i: isize) -> PaintingResult<Color> {
+        match i {
+            0 => Ok(Color::Black),
+            1 => Ok(Color::White),
+            _ => Err(PaintingError::InvalidColor(i))
+        }
+    }
+
+    fn to_int(&self) -> isize {
+        match self {
+            Color::Black => 0,
+            Color::White => 1
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Turn {
+    Left,
+    Right
+}
+
+impl Turn {
+    fn from_int(i: isize) -> PaintingResult<Turn> {
+        match i {
+            0 => Ok(Turn::Left),
+            1 => Ok(Turn::Right),
+            _ => Err(PaintingError::InvalidTurn(i))
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Facing {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+impl Facing {
+    fn leftwards(&self) -> Facing {
+        match self {
+            Facing::Up => Facing::Left,
+            Facing::Left => Facing::Down,
+            Facing::Down => Facing::Right,
+            Facing::Right => Facing::Up
+        }
+    }
+
+    fn rightwards(&self) -> Facing {
+        match self {
+            Facing::Up => Facing::Right,
+            Facing::Right => Facing::Down,
+            Facing::Down => Facing::Left,
+            Facing::Left => Facing::Up
+        }
+    }
+
+    fn turn(&self, turn: Turn) -> Facing {
+        match turn {
+            Turn::Left => self.leftwards(),
+            Turn::Right => self.rightwards()
+        }
+    }
+}
+
+#[derive(Debug)]
+struct PainterBot {
+    brain: Machine,
+    canvas: HashMap<Point, Color>,
+    location: Point,
+    facing: Facing
+}
+
+impl PainterBot {
+    fn new(machine: Machine) -> Self {
+        PainterBot {
+            brain: machine,
+            canvas: HashMap::new(),
+            location: Point { x: 0, y: 0 },
+            facing: Facing::Up
+        }
+    }
+
+    fn color_at(&self, point: &Point) -> &Color {
+        self.canvas.get(point).or(Some(&Color::Black)).unwrap()
+    }
+
+    fn run(&mut self) -> PaintingResult<()> {
+        loop {
+            self.brain.run()?;
+
+            match self.brain.state() {
+                MachineState::Halted => { return Ok(()); },
+                MachineState::Blocked => {
+                    self.brain.write(self.color_at(&self.location).to_int());
+                },
+                // Shouldn't ever get here after `run`.
+                MachineState::Running => { }
+            }
+        }
+    }
+}
 
 pub struct DayEleven {}
 
