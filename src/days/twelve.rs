aa5ae3140e75814dabc7547db08453dbc8855932
Sean Kermes <sean@arcbio.com>
2019-12-12 11:35:24 -0500

    Implement day 12 part 1

--- src/days/twelve.rs
+++ src/days/twelve.rs
@@ -59,6 +59,18 @@ impl Body {
         self.velocity.add(&delta_v);
         self.position.add(&self.velocity);
     }
+
+    fn potential_energy(&self) -> usize {
+        (self.position.x.abs() + self.position.y.abs() + self.position.z.abs()) as usize
+    }
+
+    fn kinetic_energy(&self) -> usize {
+        (self.velocity.x.abs() + self.velocity.y.abs() + self.velocity.z.abs()) as usize
+    }
+
+    fn total_energy(&self) -> usize {
+        self.potential_energy() * self.kinetic_energy()
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -98,6 +110,10 @@ impl OrbitalSystem {
             self.step();
         }
     }
+
+    fn total_energy(&self) -> usize {
+        self.bodies.iter().map(|b| b.total_energy()).sum()
+    }
 }
 
 pub struct DayTwelve {}
@@ -122,7 +138,7 @@ impl Problem for DayTwelve {
         ]);
         system.n_steps(1000);
 
-        format!("{}", "Part one not yet implemented.")
+        system.total_energy().to_string()
     }
 
     fn part_two(&self, input: &str) -> String {
@@ -133,6 +149,7 @@ impl Problem for DayTwelve {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::{assert_eq};
 
     #[test]
     fn example_1() {
@@ -166,5 +183,54 @@ mod tests {
             ],
             step_count: 1
         }, system);
+    }
+
+    #[test]
+    fn example_2() {
+        let mut system = OrbitalSystem::from_bodies(vec![
+            Body::from_triple(-8, -10, 0),
+            Body::from_triple(5, 5, 10),
+            Body::from_triple(2, -7, 3),
+            Body::from_triple(9, -8, -3)
+        ]);
+
+        system.n_steps(100);
+
+        assert_eq!(OrbitalSystem {
+            bodies: vec![
+                Body {
+                    position: Z3 { x: 8, y: -12, z: -9 },
+                    velocity: Z3 { x: -7, y: 3, z: 0 }
+                },
+                Body {
+                    position: Z3 { x: 13, y: 16, z: -3 },
+                    velocity: Z3 { x: 3, y: -11, z: -5 }
+                },
+                Body {
+                    position: Z3 { x: -29, y: -11, z: -1 },
+                    velocity: Z3 { x: -3, y: 7, z: 4 }
+                },
+                Body {
+                    position: Z3 { x: 16, y: -13, z: 23 },
+                    velocity: Z3 { x: 7, y: 1, z: 1 }
+                }
+            ],
+            step_count: 100
+        }, system);
+
+        assert_eq!(29, system.bodies[0].potential_energy());
+        assert_eq!(10, system.bodies[0].kinetic_energy());
+        assert_eq!(290, system.bodies[0].total_energy());
+        assert_eq!(32, system.bodies[1].potential_energy());
+        assert_eq!(19, system.bodies[1].kinetic_energy());
+        assert_eq!(608, system.bodies[1].total_energy());
+        assert_eq!(41, system.bodies[2].potential_energy());
+        assert_eq!(14, system.bodies[2].kinetic_energy());
+        assert_eq!(574, system.bodies[2].total_energy());
+        assert_eq!(52, system.bodies[3].potential_energy());
+        assert_eq!(9, system.bodies[3].kinetic_energy());
+        assert_eq!(468, system.bodies[3].total_energy());
+
+        assert_eq!(1940, system.total_energy());
     }
 }
