542cc5982b07810c41368343a245ad6301d07ca4
Sean Kermes <sean@arcbio.com>
2019-12-17 17:12:58 -0500

    Solve day 17 part 1

--- src/days/seventeen.rs
+++ src/days/seventeen.rs
@@ -1,10 +1,16 @@
 use crate::problem::Problem;
+use crate::intcode::Machine;
 
 pub struct DaySeventeen {}
 
 impl Problem for DaySeventeen {
     fn part_one(&self, input: &str) -> String {
-        format!("{}", "Part one not yet implemented.")
+        let mut machine = Machine::from_str(input).unwrap();
+        machine.run().unwrap();
+        for value in machine.read() {
+            print!("{}", std::char::from_u32(value as u32).unwrap());
+        }
+        "5740   (solved manually by just printing and looking)".to_string()
     }
 
     fn part_two(&self, input: &str) -> String {
