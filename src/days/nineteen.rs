a1570d428f9cb382c9669536ec1931f4d9762235
Sean Kermes <skermes@gmail.com>
2019-12-19 22:08:45 -0500

    Solve day 19 part 1

--- src/days/nineteen.rs
+++ src/days/nineteen.rs
@@ -1,10 +1,26 @@
 use crate::problem::Problem;
+use crate::intcode::Machine;
 
 pub struct DayNineteen {}
 
 impl Problem for DayNineteen {
     fn part_one(&self, input: &str) -> String {
-        format!("{}", "Part one not yet implemented.")
+        for y in 0..50 {
+            for x in 0..50 {
+                let mut machine = Machine::from_str(input).unwrap();
+                machine.write(x);
+                machine.write(y);
+                machine.run().unwrap();
+
+                match machine.read()[0] {
+                    0 => print!("."),
+                    _ => print!("#")
+                };
+            }
+            println!("");
+        }
+
+        "194    (printed and counted)".to_string()
     }
 
     fn part_two(&self, input: &str) -> String {
